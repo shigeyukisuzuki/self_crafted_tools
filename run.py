1e13c966e69cc91ae7a92e0f5c03acf60ec79e12
Suzuki Shigeyuki <kyuuritosuika@gmail.com>
2025-01-25 21:46:15 +0900

    update run.py

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
 import inotify
 
-inotify.wait(['-m', '-v', '-r', '.'])
+#outputs = inotify.wait('-m -r /tmp')
+#outputs = inotify.wait('-m -v -r /tmp')
+outputs = inotify.wait('-m -v -r -t 10 /tmp')
+#print(outputs)
+for output in outputs:
+    print(output)
 
+#outputs = inotify.wait('-r -v /tmp')
+#print(outputs)
+
+#outputs = inotify.wait("-r -v --format '%w %e' /tmp")
+#print(outputs)
